b76f1498803bcbcbe6dcf2bc0e2fad67a05b78ad
ayobamy <ayobamyahmed@gmail.com>
2023-06-07 18:16:01 +0100

    [Feat]: Created a band name generator

---
+++ Day_1/band_name_generatot.py
@@ -0,0 +1,16 @@
+# band name generator project
+
+# print welcome message
+print("Welcome to my custom band name generator 😎\n")
+
+# ask user for input (city name)
+city_name = input("What is the name of your city? 🌇\n")
+
+# ask user for input (pet name)
+pet_name = input("What is your pet's name? 🐕‍🦺\n")
+
+# combine both city and pet name
+band_name = city_name + " " + pet_name
+
+# print the band name result
+print("Your band could be " + band_name)
