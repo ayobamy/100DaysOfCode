9a0a95ff28615d6515c1e8397b4a8490c6163204
ayobamy <ayobamyahmed@gmail.com>
2023-06-07 18:15:41 +0100

    [Feat]: Added test file

---
+++ Day_1/main.py
@@ -0,0 +1,12 @@
+# print function usage
+print("Hello, Python!")
+print("This is a 'print()' function in Python")
+
+# input() function usage
+print("Hello, " + input("What is your name? "))
+print(input("What is your name? ") + " is learning Python")
+
+# variables
+name = input("What is your name? ")
+length = len(name)
+print(length)
