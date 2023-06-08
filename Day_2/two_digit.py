1c02a9fb998ce6fbd7d555d2ff59e88dbe92937e
ayobamy <ayobamyahmed@gmail.com>
2023-06-08 16:27:56 +0100

    [Feat]: Adds two-digit numbers provided by users

---
+++ Day_2/two_digit.py
@@ -0,0 +1,9 @@
+# adds a two digit number
+
+num_digits = input("Enter a two digit number: ")
+# print(type(num_digits))
+first = num_digits[0]
+second = num_digits[1]
+
+add_num = int(first) + int(second)
+print(add_num)
