eb9ce075f0cc29da044f26768692993bb1115eaf
ayobamy <ayobamyahmed@gmail.com>
2023-06-10 22:49:41 +0100

    [Feat]: created a treasure map game

---
+++ Day_4/treasure_map.py
@@ -0,0 +1,20 @@
+# treasure map
+row1 = ["😇", "😜", "🥰"]
+row2 = ["😉", "😎", "😃"]
+row3 = ["😳", "😂", "😍"]
+
+map = [row1, row2, row3]
+
+print(f"{row1}\n{row2}\n{row3}")
+
+position = input("Where do you want to put the treasure?  ")
+
+h = int(position[0])
+v = int(position[1])
+
+# selected_row = map[v - 1]
+# selected_row[h - 1] = "X"
+map[v - 1][h - 1] = "X"
+
+print(f"{row1}\n{row2}\n{row3}")
+
