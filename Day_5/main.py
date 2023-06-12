a69f62816cd6bd026619b63b89f1cd2a320d7a80
ayobamy <ayobamyahmed@gmail.com>
2023-06-12 09:53:57 +0100

    [Feat]: Added main file

---
+++ Day_5/main.py
@@ -0,0 +1,14 @@
+# Loops
+
+# for loop
+fruits = ["apple", "orange", "mango", "peach"]
+
+for fruit in fruits:
+    print(fruit)
+
+# range function
+total = 0
+for i in range(1, 101):
+    total += i
+
+print(total)
