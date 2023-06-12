62f543b575167cfb21c746d5287f7f3b14d646e0
ayobamy <ayobamyahmed@gmail.com>
2023-06-12 09:53:04 +0100

    [Feat]: wrote the fiizbuzz program

---
+++ Day_5/fizzbuzz.py
@@ -0,0 +1,12 @@
+# FizzBuzz
+# 3 == Fizz, 5 == Buzz, 3 & 5 == FizzBuzz
+
+for i in range(1, 101):
+    if (i % 3 == 0 and i % 5 == 0):
+        print("FizzBuzz")
+    elif (i % 3 == 0):
+        print("Fizz")
+    elif (i % 5 == 0):
+        print("Buzz")
+    else:
+        print(i)
